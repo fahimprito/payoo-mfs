2207ded97e9a8de6c3c8d7dd6938c93347b2ff5e
fahimprito <ahmmedahmmed75@gmail.com>
2024-09-18 23:33:45 +0600

    home

--- js/login.js
+++ js/login.js
@@ -4,14 +4,38 @@
 //     console.log("button clicked");
 // })
 
-document.getElementById("btn-login").addEventListener('click', function(event){
-    event.preventDefault();
-    console.log("button clicked");
+// document.getElementById("btn-login").addEventListener('click', function(event){
+//     event.preventDefault();
+//     console.log("button clicked");
 
+//     const phoneNumber = document.getElementById("phone-no").value;
+//     const pinNumber = document.getElementById("pin-no").value;
+
+//     console.log(phoneNumber, pinNumber);
+
+//     if(phoneNumber==="555" && pinNumber==="111"){
+//         console.log("login successful!");
+//     }
+//     else{
+//         console.log("wrong phone number or pin number!");
+
+//     }
+// })
+
+
+document.getElementById("btn-login").addEventListener('click', function (event) {
+    event.preventDefault();
 
     const phoneNumber = document.getElementById("phone-no").value;
-    console.log(phoneNumber);
-
     const pinNumber = document.getElementById("pin-no").value;
-    console.log(pinNumber);
+
+    console.log(phoneNumber, pinNumber);
+
+    if (phoneNumber === "555" && pinNumber === "111") {
+        console.log("login successful!");
+        window.location.href = "../home.html";
+    }
+    else {
+        alert("wrong phone number or pin number!")
+    }
 })
