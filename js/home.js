5839981d79b38001df62cbc74d6899b36a51ed38
fahimprito <ahmmedahmmed75@gmail.com>
2024-09-19 00:26:07 +0600

    add money

--- js/home.js
+++ js/home.js
@@ -1,1 +1,34 @@
-console.log("home added");
+
+
+// add money
+document.getElementById("btn-add-money").addEventListener('click', function (event) {
+    event.preventDefault();
+    // console.log("add-money clicked");
+
+    const addMoneyInput = document.getElementById("input-add-money").value;
+    const pinNumber = document.getElementById("pin-no").value;
+    // console.log(addMoneyInput, pinNumber);
+
+    if (pinNumber === "111") {
+        console.log("added");
+        
+        const balance = document.getElementById('account-balance').innerText;
+        // console.log(balance);
+
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = balanceNumber + addMoneyNumber;
+        console.log(newBalance);
+
+        // set value 
+        document.getElementById('account-balance').innerText = newBalance;
+
+    }
+    else {
+        alert('Failed to add money! Please try again.');
+    }
+})
+
+
+
+
