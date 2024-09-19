13face68e98752b7f3af24591f821807f3dbd3e8
fahimprito <ahmmedahmmed75@gmail.com>
2024-09-20 01:42:33 +0600

    transaction

--- js/addMoney3.js
+++ js/addMoney3.js
@@ -6,7 +6,7 @@ document.getElementById("btn-add-money").addEventListener("click", function (eve
     const addMoney = getInputFieldValueById('input-add-money');
     const pinNumber = getInputFieldValueById('pin-no');
     // console.log('amount', addMoney, 'pinNumber', pinNumber);
-    
+
 
     if (pinNumber === 111) {
         const balance = getTextFieldValueById('account-balance');
@@ -14,6 +14,13 @@ document.getElementById("btn-add-money").addEventListener("click", function (eve
 
         document.getElementById("account-balance").innerText = newBalance;
 
+        // add to transaction history
+        const p = document.createElement('p');
+        p.classList.add('bg-green-100');
+        p.innerText = `ADDED: $${addMoney}, New Balance: $${newBalance}`;
+        // console.log(p);
+
+        document.getElementById('transaction-container').appendChild(p);
     }
     else {
         alert('Failed to add money! Please try again.');
--- js/cashOut2.js
+++ js/cashOut2.js
@@ -15,7 +15,15 @@ document.getElementById("btn-cash-out").addEventListener('click', function (even
 
         document.getElementById("account-balance").innerText = newBalance;
 
+        // add to transaction history
+        const div = document.createElement('div');
+        div.classList.add('bg-red-100');
+        div.innerHTML = `
+                <h4 class="text-2xl font-bold">Cash Out</h4>
+                <p>${cashOutMoney} withdraw. New Balance ${newBalance}</p>
+            `
 
+        document.getElementById('transaction-container').appendChild(div);
     }
     else {
         alert('Failed to cash out your money! Please try again.');
